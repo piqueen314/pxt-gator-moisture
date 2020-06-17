8c79823747d03471eb5805e2857c7da3295d36f8
ceceilia hedrick <cece@MacBook-Air.local>
2020-06-16 20:21:19 -0500

    add moisture block

--- gatormoisture.ts
+++ gatormoisture.ts
@@ -1,11 +1,10 @@
 /**
-* Andy England @ SparkFun Electronics
-* September 6, 2018
-* https://github.com/sparkfun/pxt-light-bit
+* Cece Hedrick
+* June 16 2020
+* 
 
 * Development environment specifics:
 * Written in Microsoft Makecode
-* Tested with a SparkFun gatorlight sensor and micro:bit
 *
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 * Please review the LICENSE.md file included with this example. If you have any questions
@@ -15,39 +14,39 @@
 
 
 /**
- * Functions to operate the gatorlight sensor
+ * Functions to operate the gatormoisture sensor
  */
 
- enum gatorlightType{
-	 Lux=1,
+ enum gatorMoistureType{
+	 moisture=1,
 	 adcVal=2,
  }
 
 
 
 //% color=#f44242 icon="\uf185"
-namespace gatorlight {
+namespace gatorMoisture {
 
-    // Functions for reading light from the gatorlight in lux or straight adv value
+    // Functions for reading moisture from the gatormoisture in moisture or straight adv value
 
     /**
     * Reads the number
     */
-    //% weight=30 blockId="gatorlight_light" block="Get light on pin %pin | in %gatorlightType"
-    export function light(pin: AnalogPin, type: gatorlightType): number{
+    //% weight=30 blockId="gatorMoisture_moisture" block="Get moisture on pin %pin | in %gatorMoistureType"
+    export function moisture(pin: AnalogPin, type: gatorMoistureType): number{
       let ADCVal = pins.analogReadPin(pin)
       switch(type){
-        case gatorlightType.Lux: return getLux(ADCVal)
-        case gatorlightType.adcVal: return ADCVal
+        case gatorMoistureType.moisture: return getMoisture(ADCVal)
+        case gatorMoistureType.adcVal: return ADCVal
         default: return -11111111
       }
     }
 
 	/**
-     * Function used for simulator, actual implementation is in gatorlight.cpp
+     * Function used for simulator, actual implementation is in gatormoisture.cpp
      */
-    //% shim=gatorlight::getLux
-    function getLux(ADCVal: number) {
+    //% shim=gatorMoisture::getMoisture
+    function getMoisture(ADCVal: number) {
         // Fake function for simulator
         return 0
     }
